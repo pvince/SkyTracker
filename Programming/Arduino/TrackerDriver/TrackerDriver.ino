38ad3a39b9cc2f12fb57b7ac38fa293a5725ad33
Paul <pvince@gmail.com>
2012-12-06 09:13:57 -0500

    Added the 'AccelStepper' library for cleanly driving the stepper motor.

---
+++ Programming/Arduino/TrackerDriver/TrackerDriver.ino
@@ -0,0 +1,15 @@
+#define STEP_PIN 1
+#define DIR_PIN  2
+
+const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+
+int stepCount = 0;  // number of steps the motor has taken
+
+void setup() {
+  // nothing to do inside the setup
+}
+
+void loop() {
+}
+
+
